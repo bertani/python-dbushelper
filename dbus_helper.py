0180cafd98fef2601a11212736c6aaf1db3e78c1
Thomas Bertani <sylar@anche.no>
2010-04-09 19:01:26 +0200

    added args support to methods

--- dbus_helper.py
+++ dbus_helper.py
@@ -1,31 +1,41 @@
 #!/usr/bin/env python
 
-import dbus
+from dbus import SystemBus, Array
 import xml.etree.ElementTree as et
 from new import instancemethod
-from exceptions import Exception
 
 class Helper:
     __utils__ = {}
     def __init__(self, path):
-        def addMethod(name):
-            setattr(self, name, instancemethod(lambda x: x.__utils__['obj'].get_dbus_method(name).__call__(), self, self.__class__))
-        self.__utils__['bus'] = dbus.SystemBus()
+        def addMethod(name, signature="sv", args=()):
+            def f(x, *args):
+                try:
+                    res = x.__utils__['obj'].get_dbus_method(name, path)(*args)
+                    return res
+                except TypeError: return "Wrong arguments passed.."
+            f.func_name = name
+            doc = "Usage: %s(" % f.func_name
+            for arg in args: doc += "%s," % arg
+            doc = doc[:-1] + ")"
+            f.func_doc = doc
+            setattr(self, name, instancemethod(f, self, self.__class__))
+        self.__utils__['bus'] = SystemBus()
         if not path in self.__utils__['bus'].list_names():
-            raise Exception("Cannot load path '%s'" % path)
+            print "Cannot load path '%s'" % path
+            return
         self.__utils__['obj'] = self.__utils__['bus'].get_object(path, "/")
-        xml = et.fromstring(self.__utils__['obj'].Introspect())
+        xml = self.__utils__['obj'].Introspect()
+        xml = et.fromstring(xml)
         res = None
         for interface in xml.findall("interface"):
-            for name, value in interface.items():
-                if name == "name" and value == path: res = interface
+            res = interface if interface.get("name") == path else None
             if res: break
         for children in res._children:
-            _name = ""
-            for name, value in children.items():
-                if name == "name": _name = value
-            addMethod(_name)
-
-if __name__ == "__main__":
-    a = Helper("org.neophysis.nde")
-    print dir(a)
+            method_name, signature = "", ""
+            args = []
+            method_name = children.get("name")
+            for c in children._children:
+                if c.tag == "arg":
+                    if c.get("direction") == "in": args.append(c.get("name"))
+                    else: signature = c.get("type")
+            addMethod(method_name, signature, args)
