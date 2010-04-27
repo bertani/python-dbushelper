4fc8a1422f3b18bd0924796101544c363ad08544
Thomas Bertani <sylar@anche.no>
2010-04-27 06:34:08 +0200

    now the methods of all interfaces are available even if there is more than one of them in the specified dbus object

--- dbus_helper.py
+++ dbus_helper.py
@@ -33,16 +33,16 @@ class Helper:
     
     Example: Helper("org.neophysis.nwm") or just Helper("nwm")
     '''
-    __utils__ = {'bus': None, 'obj': None, 'interface': None}
+    __utils__ = {'bus': None, 'obj': None}
     def __init__(self, path, obj="/"):
-        def addMethod(name, args=()):
+        def addMethod(name, interface_name, single_interface, args=()):
             def f(x, *args):
                 try:
-                    return x.__utils__['obj'].get_dbus_method(name, self.__utils__['interface'].get("name"))(*args)
+                    return x.__utils__['obj'].get_dbus_method(name, interface_name)(*args)
                 except TypeError: raise Exception("Wrong arguments passed..")
-            f.func_name = name
+            f.func_name = "%s_%s" % (interface_name.split(".")[-1], name) if not single_interface else name
             f.func_doc = "Usage: %s(%s)" % (f.func_name or "f", ', '.join(arg if arg else chr(ord('a')+n) for n, arg in enumerate(args)))
-            setattr(self, name, instancemethod(f, self, self.__class__))
+            setattr(self, f.func_name, instancemethod(f, self, self.__class__))
         self.__utils__['bus'] = SystemBus()
         r = []
         for i in self.__utils__['bus'].list_names():
@@ -53,13 +53,14 @@ class Helper:
         self.__utils__['obj'] = self.__utils__['bus'].get_object(path, obj)
         try: xml = fromstring(Interface(self.__utils__['obj'], "org.freedesktop.DBus.Introspectable").Introspect())
         except: raise Exception("Introspection error")
+        if len(xml.findall("interface")) == 1: single_interface = True
+        else: single_interface = False
         for interface in xml.findall("interface"):
-            self.__utils__['interface'] = interface if interface.get("name").startswith(path) else None if not self.__utils__['interface'] else interface
-        if not self.__utils__['interface']: raise Exception("Cannot find any interface which matchs the '%s' path" % path)
-        for children in self.__utils__['interface']._children:
-            args = []
-            if children.tag != "method": continue
-            method_name = children.get("name")
-            for c in children._children:
-                if (c.tag == "arg") and (c.get("direction") == "in"): args.append(c.get("name"))
-            addMethod(method_name, args)
+            interface_name = interface.get("name")
+            for children in interface._children:
+                args = []
+                if children.tag != "method": continue
+                method_name = children.get("name")
+                for c in children._children:
+                    if (c.tag == "arg") and (c.get("direction") == "in"): args.append(c.get("name"))
+                addMethod(method_name, interface_name, single_interface, args)
